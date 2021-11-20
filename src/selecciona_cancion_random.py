6f75e01a6e6e931f65c175e2512bd72eceb5113c
ErikPC <epardillo@cifpfboll.eu>
2021-11-20 18:47:58 +0100

    añadido asserts dentro del modulo selecciona_cancion_random

--- src/selecciona_cancion_random.py
+++ src/selecciona_cancion_random.py
@@ -1,10 +1,13 @@
 import random
+from typing import List
 
 
 def inicia_cache():
     cache = []
 
     def selecciona_cancion_random(libreria):
+        assert isinstance(libreria, dict)
+
         nonlocal cache
 
         canciones_restantes = [cancion for cancion in libreria if cancion not in cache]
@@ -12,6 +15,10 @@ def inicia_cache():
         cancion_seleccionada = random.choice(canciones_restantes)
 
         cache.append(cancion_seleccionada)
+
+        assert isinstance(cancion_seleccionada, str)
+        assert cancion_seleccionada in libreria
+
         return cancion_seleccionada
 
     return selecciona_cancion_random
