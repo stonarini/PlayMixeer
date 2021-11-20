4c890f878d4d23874db9813b19a5f24d01d79d42
ErikPC <epardillo@cifpfboll.eu>
2021-11-20 19:51:31 +0100

    añadido asserts de control en play_shuffle

--- src/play_shuffle.py
+++ src/play_shuffle.py
@@ -2,9 +2,15 @@ from src.selecciona_cancion_random import inicia_cache
 
 
 def play_shuffle(libreria):
+
+    assert isinstance(libreria, dict)
+
     playlist = {}
     selecciona_cancion_random = inicia_cache()
     for llave_index in range(1, len(libreria)+1):
         playlist[llave_index] = selecciona_cancion_random(libreria)
         print("Seleccionada: " + playlist[llave_index])
+
+    assert isinstance(playlist, dict)
+
     return playlist
