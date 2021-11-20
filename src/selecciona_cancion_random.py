201ef450da3f22340ad590ab14c9c5e2e267de31
stonarini <stonarini@cifpfbmoll.eu>
2021-11-20 13:27:04 +0100

    mejorado test cancion_random y mejoras en general

--- src/selecciona_cancion_random.py
+++ src/selecciona_cancion_random.py
@@ -1,16 +1,17 @@
 import random
 
-# si no pasan nada es []
 
-
-def inicia_cache(canciones_conocidas=[]):
-    cache = canciones_conocidas
+def inicia_cache():
+    cache = []
 
     def selecciona_cancion_random(libreria):
         nonlocal cache
+
         canciones_restantes = [cancion for cancion in libreria if cancion not in cache]
         # canciones_restantes = list(set(libreria) - set(cache))
         cancion_seleccionada = random.choice(canciones_restantes)
+
         cache.append(cancion_seleccionada)
         return cancion_seleccionada
+
     return selecciona_cancion_random
--- test/test_selecciona_cancion_random.py
+++ test/test_selecciona_cancion_random.py
@@ -1,8 +1,13 @@
 import pytest
 from src.selecciona_cancion_random import inicia_cache
 
-libreria = {"Cancion1": {}, "Lady gaga": {}, "pesce in mano": {}}
+libreria = {"Cancion1": {}, "Lady gaga": {}, "pesce in mano": {}, "roxxane": {}, "nata vota": {}}
 
+
+
+@pytest.mark.test_comprobar_funcion
+def test_comprobar_funcion():
+    assert callable(inicia_cache())
 
 @pytest.mark.test_cancion_en_libreria
 def test_cancion_en_libreria():
@@ -16,6 +21,12 @@ def test_cancion_es_cancion():
 
 @pytest.mark.test_salida_random
 def test_salida_random():
-    seleccionaCancionRandom = inicia_cache()
-    playlist_generada = [seleccionaCancionRandom(libreria), seleccionaCancionRandom(libreria), seleccionaCancionRandom(libreria)]
-    assert playlist_generada != libreria.keys()
+    selecciona_cancion_random = inicia_cache()
+    primera_playlist = [selecciona_cancion_random(libreria) for cancion in libreria]
+
+    selecciona_cancion_random = inicia_cache() # reinicia cache
+    segunda_playlist = [selecciona_cancion_random(libreria) for cancion in libreria]
+
+    selecciona_cancion_random = inicia_cache() # reinicia cache
+    tercera_playlist = [selecciona_cancion_random(libreria) for cancion in libreria]
+    assert primera_playlist != segunda_playlist and primera_playlist != tercera_playlist
