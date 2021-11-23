dd8e65ff5b3189d9a585d6b90b99c964f6cd270e
ErikPC <epardillo@cifpfboll.eu>
2021-11-23 11:04:36 +0100

    Añadidos asserts en crear_comando

--- src/lanzar_VLC/crear_comando.py
+++ src/lanzar_VLC/crear_comando.py
@@ -3,11 +3,16 @@ import os
 
 
 def crear_comando(libreria, playlist):
+    assert isinstance(libreria, dict)
+    assert isinstance(playlist, dict)
     if sys.platform == "win32":
         comando = ["C:/Program File/VideoLAN/VLC/vlc.exe", "-I", "dummy", "--dummy-quiet"]
     else:
         comando = ["/usr/bin/cvlc"]
+    assert isinstance(comando, list)
     for numero_cancion, titulo_cancion in sorted(playlist.items()):
+        assert isinstance(titulo_cancion, str)
+        assert isinstance(numero_cancion, int)
         try:
             ruta_cancion = libreria[titulo_cancion]["location"]
         except KeyError:
